1f3b6a2adc4bb9c2bc19700721bc8ac5930b4559
markulT <marrcokyr@gmail.com>
2025-10-19 20:01:30 +0200

    disko configuration fixed

--- disko-config.nix
+++ disko-config.nix
@@ -1,34 +1,34 @@
 # /etc/nixos/disko-config.nix
-
-{ lib, pkgs, ... }:
-
+{ lib, ... }:
 {
-  # This uses the disko tool to declaratively partition the disk.
   disko.devices = {
-    disk.main = {
-      # This assumes your VM's disk is /dev/sda. Change if necessary.
-      device = "/dev/sda";
-      type = "gpt";
-      partitions = [
-        {
-          name = "boot";
-          size = "512M";
-          content = {
-            type = "filesystem";
-            format = "vfat"; # vfat is fat32
-            mountpoint = "/boot";
+    disk = {
+      main = {
+        device = "/dev/sda";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              size = "512M";
+              type = "EF00";  # EFI system partition
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
           };
-        }
-        {
-          name = "root";
-          size = "100%";
-          content = {
-            type = "filesystem";
-            format = "ext4";
-            mountpoint = "/";
-          };
-        }
-      ];
+        };
+      };
     };
   };
 }
